63920c22a74954726ece9bf93becd27d3ff9f40e
Yaroslav Ivasenko <yarik.dock@gmail.com>
2026-01-04 16:23:43 +0200

    pd

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,5 +1,5 @@
 export const UserInfo = ({ user }) => (
-  <a className="UserInfo" href={`mailto:${user.email}`} key={user.key}>
+  <a className="UserInfo" href={`mailto:${user.email}`}>
     {user.name}
   </a>
 );
